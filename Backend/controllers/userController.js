188e3895bccd8cb390edebb9b611c6b47a5db784
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-10-31 23:58:47 +0530

    User Model Defined

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,7 @@
+import UserModel from '../models/User.js'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+class UserController {
+  static userRegistration = async (req, res) => {}
+}
---
+++ Backend/models/User.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+
+// Define Schema for User
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+})
+
+// Creating and Exporting the Model
+const UserModel = mongoose.model('User', UserSchema)
+export default UserModel
