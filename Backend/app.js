8cb767017efbfaf4143857ed160c8f21e71f88aa
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-11-01 18:40:39 +0530

    Project Completed

--- Backend/app.js
+++ Backend/app.js
@@ -22,5 +22,5 @@ app.use(express.json())
 app.use('/api/user', userRoutes)
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
+  console.log(`Your backend server is running on http://localhost:${PORT}`)
 })
