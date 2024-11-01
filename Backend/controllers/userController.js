fdbca608d9af1758226efc5ed3a8378c5c78866b
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-11-01 17:24:37 +0530

    Email Verification Done

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import UserModel from '../models/User.js'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import transporter from '../config/email-config.js'
 
 class UserController {
   // Registration method for creating a new user
@@ -138,31 +139,42 @@ class UserController {
 
   // Change password method
   static changeUserPassword = async (req, res) => {
+    // Destructure password and password_confirmation from the request body
     const { password, password_confirmation } = req.body
 
+    // Check if both password and password_confirmation fields are provided
     if (password && password_confirmation) {
+      // Check if the passwords match
       if (password === password_confirmation) {
+        // Generate a salt for hashing the password
         const salt = await bcrypt.genSalt(12)
+        // Hash the new password with the generated salt
         const newHashedPassword = await bcrypt.hash(password, salt)
+
+        // Update the user's password in the database using their user ID
         await UserModel.findByIdAndUpdate(req.user._id, {
           $set: {
-            password: newHashedPassword,
+            password: newHashedPassword, // Set the new hashed password
           },
         })
+
+        // Send a success response indicating the password has been changed
         res.status(200).send({
           status: 'success',
           message: 'Password changed successfully',
         })
       } else {
+        // Send an error response if the passwords do not match
         return res.status(400).send({
           status: 'failed',
-          message: 'Passwords do not match',
+          message: 'Passwords do not match', // Inform the user about the mismatch
         })
       }
     } else {
+      // Send an error response if any of the fields are missing
       return res.status(400).send({
         status: 'failed',
-        message: 'All fields are required',
+        message: 'All fields are required', // Inform the user that both fields are necessary
       })
     }
   }
@@ -170,6 +182,108 @@ class UserController {
   // Path to get the information of a logged user
   static loggedUser = async (req, res) => {
     res.send({ user: req.user })
+  }
+
+  // Path to send the email to reset the password
+  static sendUserPasswordResetEmail = async (req, res) => {
+    // Destructure email from the request body
+    const { email } = req.body
+
+    // Check if the email field is provided
+    if (email) {
+      // Search for a user in the database with the provided email
+      const user = await UserModel.findOne({ email })
+
+      // If a user is found with the given email
+      if (user) {
+        // Create a secret using the user's ID and the JWT secret key from the environment variables
+        const secret = user._id + process.env.JWT_SECRET_KEY
+
+        // Generate a token for password reset with an expiration time of 15 minutes
+        const token = jwt.sign(
+          {
+            userId: user._id, // Include the user's ID in the token payload
+          },
+          secret, // Use the secret to sign the token
+          {
+            expiresIn: '15m', // Set the token to expire in 15 minutes
+          },
+        )
+
+        // Create a password reset link containing the user ID and token
+        const link = `http://127.0.0.1:3000/api/user/reset/${user._id}/${token}`
+        console.log(link)
+
+        // Sending the email to the user with the link to reset the password
+        let info = await transporter.sendMail({
+          from: process.env.EMAIL_FROM, //  The sender's(you) email
+          to: user.email, // The recipient's email
+          subject: 'Reset Password Link', // The subject of the email
+          html: `<h2>Click the link below to reset your password.</h2> <br/> <a href=${link} target="_blank">Reset Password</a>`,
+        })
+
+        // Send a success response indicating the email has been sent
+        res.status(200).send({
+          status: 'success',
+          message: 'Email sent successfully', // Notify the user that the email was sent
+          info: info,
+        })
+      } else {
+        // Return an error response if no user is found with the entered email
+        return res.status(400).send({
+          status: 'failed',
+          message: 'User with the entered email not found', // Inform the user that the email is not associated with any account
+        })
+      }
+    } else {
+      // Return an error response if the email field is missing
+      return res.status(400).send({
+        status: 'failed',
+        message: 'Email is required', // Inform the user that the email is a required field
+      })
+    }
+  }
+
+  // Path to reset password
+  static userPasswordRest = async (req, res) => {
+    const { password, password_confirmation } = req.body
+    const { id, token } = req.params
+    const user = await UserModel.findById(id)
+    const newSecret = user._id + process.env.JWT_SECRET_KEY
+
+    try {
+      jwt.verify(token, newSecret)
+      if (password && password_confirmation) {
+        if (password === password_confirmation) {
+          const salt = await bcrypt.genSalt(12)
+          const hashedPassword = await bcrypt.hash(password, salt)
+          await UserModel.findByIdAndUpdate(user._id, {
+            $set: {
+              password: hashedPassword,
+            },
+          })
+          return res.status(200).send({
+            status: 'success',
+            message: 'Password updated successfully', // Notify the user that the password was updated
+          })
+        } else {
+          return res.status(400).send({
+            status: 'failed',
+            message: 'Passwords do not match', // Inform the user that the passwords do not match
+          })
+        }
+      } else {
+        return res.status(400).send({
+          status: 'failed',
+          message: 'Password and password confirmation are required', // Inform the user that the password and password
+        })
+      }
+    } catch (error) {
+      return res.status(400).send({
+        status: 'failed',
+        message: 'Invalid token', // Inform the user that the token is invalid
+      })
+    }
   }
 }
 
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -11,6 +11,11 @@ router.use('/loggeduser', checkUserAuth)
 // Public Routes
 router.post('/register', UserController.userRegistration)
 router.post('/login', UserController.userLogin)
+router.post(
+  '/send-reset-password-email',
+  UserController.sendUserPasswordResetEmail,
+)
+router.post('/reset-password/:id/:token', UserController.userPasswordRest)
 
 // Protected Routes
 router.post('/changepassword', UserController.changeUserPassword)
